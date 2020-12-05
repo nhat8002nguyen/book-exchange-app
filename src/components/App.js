67963dc5535a8ed0b1cd685e0a1084cf0324c1de
nhat8002nguyen <nv.nhat8002@gmail.com>
2020-12-05 13:41:20 +0700

    add card view

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,47 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import BookCardView from "./BookCardView";
 
-function App() {}
+function App() {
+  return (
+    <div>
+      <table>
+        <tr>
+          <td>
+            <BookCardView />
+          </td>
+          <td>
+            <BookCardView />
+          </td>
+          <td>
+            <BookCardView />
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <BookCardView />
+          </td>
+          <td>
+            <BookCardView />
+          </td>
+          <td>
+            <BookCardView />
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <BookCardView />
+          </td>
+          <td>
+            <BookCardView />
+          </td>
+          <td>
+            <BookCardView />
+          </td>
+        </tr>
+      </table>
+    </div>
+  );
+}
 
 export default App;
